bf28508595c66a522abc533c43508d61483d1962
Erik Wendel <erik@wendel.no>
2013-11-08 13:30:22 +0100

    created report folder

---
+++ report/report.js
@@ -0,0 +1,6 @@
+$(function() {
+	var r = Raphael(10, 50, 640, 480);
+	// Creates pie chart at with center at 320, 200,
+	// radius 100 and data: [55, 20, 13, 32, 5, 1, 2]
+	r.piechart(320, 240, 100, [55, 20, 13, 32, 5, 1, 2]);
+});
