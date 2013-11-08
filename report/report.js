6fc393824530bf36970a30e5d1b42e93ad31394d
Erik Wendel <erik@wendel.no>
2013-11-08 13:59:12 +0100

    added graphael and polling each 1000ms

--- report/report.js
+++ report/report.js
@@ -1,6 +1,41 @@
-$(function() {
-	var r = Raphael(10, 50, 640, 480);
-	// Creates pie chart at with center at 320, 200,
-	// radius 100 and data: [55, 20, 13, 32, 5, 1, 2]
-	r.piechart(320, 240, 100, [55, 20, 13, 32, 5, 1, 2]);
-});
+var r;
+
+
+var x = 10;
+var y = 10;
+var canvasW = 800;
+var canvasH = 600;
+
+var chartW = 200;
+var chartH = 200;
+
+var timerId;
+
+function init() {
+	r = Raphael(x, y, canvasW, canvasH);
+	getData();
+}
+
+$(init);
+
+function getData(callback) {
+	$.get('/data', function(data, response, jqXHR) {
+		renderChart(getVotes(JSON.parse(data)));
+		console.log('got response from server');
+	});
+}
+
+function getVotes(items) {
+	var scores = [];
+	for (var i = items.length - 1; i >= 0; i--) {
+		var item = items[i];
+		scores.push(item.vote);
+	};
+	return scores;
+}
+
+function renderChart(scores) {
+	console.log(scores);
+	r.barchart(x, y, chartW, chartH, scores, { stacked: true });
+	timerId = setTimeout(getData, 1000);
+}
