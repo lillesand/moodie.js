4e6578f0e46a782e43ba43ad1ebcc7a171150b83
Erik Wendel <erik@wendel.no>
2013-11-11 10:00:08 +0100

    refactored report.js

--- report/report.js
+++ report/report.js
@@ -1,17 +1,7 @@
 var chart;
-
-var x = 10;
-var y = 10;
-var canvasW = 800;
-var canvasH = 600;
-
-var chartW = 200;
-var chartH = 200;
-
 var timerId;
 
 function init() {
-
 	var context = $("#barChart").get(0).getContext("2d");
 	chart = new Chart(context);
 	getData();
@@ -22,33 +12,33 @@ $(init);
 function getData(callback) {
 	$.get('/data', function(data, response, jqXHR) {
 		renderChart(JSON.parse(data));
-		});
+	});
 	timerId = setTimeout(getData, 1000);
-}
+};
 
 function getVoteCountForScore(votes, score) {
 	return _.countBy(votes, function(vote) { return vote.vote === score; }).true;
-}
+};
 
 function renderChart(items) {
-	var lowScores = getVoteCountForScore(items, 1);
-	var mediumScores = getVoteCountForScore(items, 2);
-	var highScores = getVoteCountForScore(items, 3);
+	var lowScoreCount = getVoteCountForScore(items, 1);
+	var mediumScoreCount = getVoteCountForScore(items, 2);
+	var highScoreCount = getVoteCountForScore(items, 3);
 
 	var data = {
-	labels : [":(",":|",":)"],
-	datasets : [
+		labels : [":(",":|",":)"],
+		datasets : [
 			{
 				fillColor : "rgba(220,220,220,0.5)",
 				strokeColor : "rgba(220,220,220,1)",
-				data : [lowScores, mediumScores, highScores]
+				data : [lowScoreCount, mediumScoreCount, highScoreCount]
 			}
 		]
-	}
+	};
 
 	var options = {
 		scaleOverride : true,
-		scaleSteps : _.max([lowScores, mediumScores, highScores]),
+		scaleSteps : _.max([lowScoreCount, mediumScoreCount, highScoreCount]),
 		scaleStepWidth : 1,
 		scaleStartValue : 0,
 		animation: false
@@ -56,4 +46,4 @@ function renderChart(items) {
 	};
 
 	chart.Bar(data, options);
-}
+};
